6bce4885da4db2f30f0c34ba4c45c99ee2fdcffa
Alberto Saltarelli <a.saltarelli93@gmail.com>
2021-11-23 18:26:04 +0100

    Add server implementation

--- lab-exams/server/src/api.h
+++ lab-exams/server/src/api.h
@@ -8,6 +8,16 @@
 #ifndef API_H_
 #define API_H_
 
+#define LOCAL_HOST "127.0.0.1"
+#define PROTOPORT 60000 // default protocol port number
+#define QLEN 5 // size of request queue (server is not multi-threading, in this example)
 
+typedef struct calc_operation {
+	char operator; // +, -, /, *
+	int op1;
+	int op2;
+} calc_operation;
+
+typedef float calc_result;
 
 #endif /* API_H_ */
