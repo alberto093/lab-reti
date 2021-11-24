3f7af8e8ae126f5a59b8fd0f661306cb7df35171
Alberto Saltarelli <a.saltarelli93@gmail.com>
2021-11-24 21:20:48 +0100

    Improve api header

--- lab-exams/client/src/api.h
+++ lab-exams/client/src/api.h
@@ -8,16 +8,31 @@
 #ifndef API_H_
 #define API_H_
 
+#if defined WIN32
+#include <winsock.h>
+#else
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#define closesocket close
+#endif
+
 #define LOCAL_HOST "127.0.0.1"
 #define PROTOPORT 60000 // default protocol port number
 #define QLEN 5 // size of request queue (server is not multi-threading, in this example)
 
 typedef struct calc_operation {
-	char operator; // +, -, /, *
-	int op1;
-	int op2;
+    char operator; // +, -, /, *
+    int op1;
+    int op2;
 } calc_operation;
 
 typedef float calc_result;
+
+void clearwinsock(void);
 
 #endif /* API_H_ */
